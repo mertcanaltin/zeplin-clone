d1d223354ba464840f2333dafedd0c02e81babd7
Mert Can Altın <mertgold60@gmail.com>
2022-08-27 14:59:42 +0300

    created logos area

---
+++ components/logos-area/index.tsx
@@ -0,0 +1,36 @@
+
+
+const LogoArea = () => (
+    <>
+    <div className="logos">
+       <div className="logo"> 
+            <img className="" src="https://cdn.sanity.io/images/wd3e2pma/production/f948a7ea62ce1cae5fdc0f8de5a1878ee6158c30-131x64.svg" width="131" height="64" alt="HSBC Logo"/>
+       </div>
+       <div className="logo"> 
+            <img className="" src="https://cdn.sanity.io/images/wd3e2pma/production/3e945e1f489ae8d4ee4a7ba634b03eeb9537bd3d-91x64.svg" width="131" height="64" alt="HSBC Logo"/>
+       </div>
+       <div className="logo"> 
+            <img className="" src="https://cdn.sanity.io/images/wd3e2pma/production/37b467ca0a8c02fe0fc60500d904076fe42339ea-64x64.svg" width="131" height="64" alt="HSBC Logo"/>
+       </div>
+       <div className="logo"> 
+            <img className="" src="https://cdn.sanity.io/images/wd3e2pma/production/328694735449bfa1377b268c8998e8c78e6886b2-65x64.svg" width="131" height="64" alt="HSBC Logo"/>
+       </div>
+       <div className="logo"> 
+            <img className="" src="https://cdn.sanity.io/images/wd3e2pma/production/08013dddfbe8ca309037dc4232e0081a48d76bc0-142x64.svg" width="131" height="64" alt="HSBC Logo"/>
+       </div>
+       <div className="logo"> 
+            <img className="" src="https://cdn.sanity.io/images/wd3e2pma/production/681102c2238bc20f2d7d3b27c30015c610dc01b9-188x64.svg" width="131" height="64" alt="HSBC Logo"/>
+       </div>
+    </div>
+
+    <div className="a-container">
+        <a href="https://blog.zeplin.io/customer-stories">
+        Explore why leading product teams use Zeplin
+        </a>
+    </div>
+    </>
+    
+   )
+   
+export default LogoArea
+   
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Header from "../components/header"
 import Banner from "../components/banner"
 import Main from "../components/main"
+import LogoArea from "../components/logos-area"
 
 const IndexPage = () => (
   <>
     <Header/>
     <Banner/>
     <Main/>
+    <LogoArea/>
   </>
 
 )
